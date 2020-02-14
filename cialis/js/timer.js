f64a9fafccfddeaba1e54b5ad9e442bfb6bb74b6
Naraxnn <51683911+Naraxnn@users.noreply.github.com>
2020-02-14 10:40:06 +0300

    Добавлен новый сайт с целью обучения

---
+++ cialis/js/timer.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function () {
+    jQuery(".eTimer").eTimer({
+        etType: 0,
+        etDate: "18.02.2020.0.0",
+        etTitleText: "",
+        etTitleSize: 20,
+        etShowSign: 1,
+        etSep: ":",
+        etFontFamily: "Trebuchet MS",
+        etTextColor: "#a3a3a3",
+        etPaddingTB: 15,
+        etPaddingLR: 15,
+        etBackground: "",
+        etBorderSize: 0,
+        etBorderRadius: 2,
+        etBorderColor: "white",
+
+        etLastUnit: 4,
+        etNumberFontFamily: "Impact",
+        etNumberSize: 40,
+        etNumberColor: "white",
+        etNumberPaddingTB: 0,
+        etNumberPaddingLR: 8,
+        etNumberBackground: "#000",
+        etNumberBorderSize: 0,
+        etNumberBorderRadius: 5,
+        etNumberBorderColor: "white",
+
+    });
+});
